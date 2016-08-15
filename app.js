f513ec160a2ad0a0bcd6a784c9d25cafa0e834da
sebtn <sebtn@users.noreply.github.com>
2016-08-15 13:44:37 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+  $('#search-button').on('click', function(){
+    var inputText = $('#search-box').val()
+  
+    $.post('https://en.wikipedia.org/w/api.php?action=query&generator=search&utf8=1&gsrsearch=' + inputText +'&prop=extracts&exintro=1&exlimit=20&exchars=200&format=json&origin=*', function(data){
+      var wikisObj = (data.query.pages)
+      var ul = $('<ul/>')
+      $.each(wikisObj, function(key, value) {
+
+      ul.append('<li class=' + value.title + '><a href=https://en.wikipedia.org/?curid=' +  value.pageid + '>' + value.extract + '</li>' ) 
+      
+      })
+      $('#wiki-box').html(ul)
+    })    
+  })
+  $('form input').on('keypress', function(e) {
+    return e.which !== 13;
+  });
+  
+}) 
